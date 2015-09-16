d3a97edb59864cc6f8dbcb6638337b5a24e5a184
broomweed <milesabaker@gmail.com>
2015-09-16 13:49:55 -0700

    Updated compilation for Linux.
    Since I'm back on Linux now.

--- Makefile.nix
+++ Makefile.nix
@@ -1,14 +1,17 @@
 CC=g++
 CFLAGS=-lsfml-graphics -lsfml-window -lsfml-system -std=c++11
 
-window_open: window_open.o
-	$(CC) window_open.o SceneManager.o -g -o window_open $(CFLAGS)
+window_open: window_open.o Scene.o SceneManager.o
+	$(CC) window_open.o Scene.o SceneManager.o -g -o window_open $(CFLAGS)
 
 window_open.o: window_open.cpp TextBox.hpp MenuTextBox.hpp DialogueTextBox.hpp TileMap.hpp GameObject.hpp Scene.hpp Animation.hpp SpriteSheet.hpp Character.hpp Thing.hpp Door.hpp Dialogue.hpp SceneManager.o BorderStyle.hpp Font.hpp Building.hpp
 	$(CC) -g -c window_open.cpp $(CFLAGS)
 
 SceneManager.o: SceneManager.cpp SceneManager.hpp
 	$(CC) -g -c SceneManager.cpp $(CFLAGS)
+
+Scene.o: Scene.cpp Scene.hpp
+	$(CC) -g -c Scene.cpp $(CFLAGS)
 
 min: window_open_min.o
 	$(CC) window_open_min.o -g -o window_open_min $(CFLAGS)
